807f1e50dc1676e52bc7fdafd94f8e1096f5d917
DineshGandham <dineshgandham99@gmail.com>
2025-06-26 18:11:10 +0530

    updated with new problems

--- patterns/SquarePattern.java
+++ patterns/SquarePattern.java
@@ -19,6 +19,12 @@ public class SquarePattern {
      * This method prints a hollow square pattern of asterisks.
      * The size of the square is determined by the input parameter n.
      */
+
+    // * * * * * 
+    // *       * 
+    // *       * 
+    // *       * 
+    // * * * * * 
     public void printHollowSquarePattern(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
